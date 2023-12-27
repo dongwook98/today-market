ded518cf64a5cfbf74872f015e94735d117c4fcb
강동욱 <ehddnr8563@gmail.com>
2023-12-28 03:28:00 +0900

    add: firebase google login 구현

---
+++ src/api/firebase.js
@@ -0,0 +1,37 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export function login() {
+  return signInWithPopup(auth, provider).catch(console.error);
+}
+
+export function logout() {
+  return signOut(auth).catch(console.error);
+}
+
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, (user) => {
+    // 1. 사용자가 있는 경우에 (로그인한경우)
+    // 2. 사용자가 어드민 권한을 가지고 있는지 확인!
+    // 3. {...user, isAdmin: true/false}
+    callback(user);
+  });
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoToday } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
+import { login, logout, onUserStateChange } from '../api/firebase';
+import User from './User';
 
 export default function Header() {
+  const [user, setUser] = useState();
+  const [admin, setAdmin] = useState();
+  console.log(user);
+  console.log('admin', admin);
+  useEffect(() => {
+    onUserStateChange(
+      (user) => setUser(user),
+      (user) => setAdmin({ ...user, admin: user.uid })
+    );
+  }, []);
+
   return (
     <header className='flex justify-between border-b border-slate-200 p-4'>
       <Link to='/' className='flex items-center text-4xl text-brand'>
         <IoToday />
-        <h1 className='ml-2 text-3xl font-bold'>오늘마켓</h1>
+        <h1 className='ml-2 font-bold hidden md:block'>오늘마켓</h1>
       </Link>
-      <nav className='flex items-center gap-x-8 font-semibold'>
+      <nav className='flex items-center gap-x-4 md:gap-x-6 font-semibold'>
         <Link to='/products'>상품 구경하기</Link>
-        <Link to='/carts'>장바구니</Link>
-        <Link to='/products/new'>상품 추가하기</Link>
-        <button>로그인</button>
+        {user && (
+          <>
+            <Link to='/carts'>장바구니</Link>
+            <Link to='/products/new'>상품 추가하기</Link>
+            {user && <User user={user} />}
+          </>
+        )}
+        {!user && <button onClick={login}>로그인</button>}
+        {user && <button onClick={logout}>로그아웃</button>}
       </nav>
     </header>
   );
