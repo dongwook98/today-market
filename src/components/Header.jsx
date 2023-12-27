62adf3c18d767028ec83a69207fd8377fc16138f
강동욱 <ehddnr8563@gmail.com>
2023-12-28 01:27:41 +0900

    update: 헤더 UI 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='flex justify-between p-4 border-b border-slate-200'>
-      <Link to='/' className='flex text-4xl text-brand'>
+    <header className='flex justify-between border-b border-slate-200 p-4'>
+      <Link to='/' className='flex items-center text-4xl text-brand'>
         <IoToday />
         <h1 className='ml-2 text-3xl font-bold'>오늘마켓</h1>
       </Link>
-      <nav className='flex gap-x-8 items-center font-bold'>
+      <nav className='flex items-center gap-x-8 font-semibold'>
         <Link to='/products'>상품 구경하기</Link>
         <Link to='/carts'>장바구니</Link>
         <Link to='/products/new'>상품 추가하기</Link>
