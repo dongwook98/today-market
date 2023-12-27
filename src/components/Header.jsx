94f4c8ecf1576eccff1af911323530c2b4a68d58
강동욱 <ehddnr8563@gmail.com>
2023-12-28 06:03:54 +0900

    add: 어드민 사용자 UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { IoToday } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 import { login, logout, onUserStateChange } from '../api/firebase';
+import Button from './ui/Button';
 import User from './User';
 
 export default function Header() {
@@ -22,15 +23,11 @@ export default function Header() {
       </Link>
       <nav className='flex items-center gap-x-4 md:gap-x-6 font-semibold'>
         <Link to='/products'>상품 구경하기</Link>
-        {user && (
-          <>
-            <Link to='/carts'>장바구니</Link>
-            {user.isAdmin && <Link to='/products/new'>상품 추가하기</Link>}
-            {user && <User user={user} />}
-          </>
-        )}
-        {!user && <button onClick={login}>로그인</button>}
-        {user && <button onClick={logout}>로그아웃</button>}
+        {user && <Link to='/carts'>장바구니</Link>}
+        {user && user.isAdmin && <Link to='/products/new'>상품 추가하기</Link>}
+        {user && <User user={user} />}
+        {!user && <Button text='로그인' onClick={login} />}
+        {user && <Button text='로그아웃' onClick={logout} />}
       </nav>
     </header>
   );
