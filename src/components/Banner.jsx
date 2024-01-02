7aca1187df823e80b6aa9af2a05b4503d4b983f4
강동욱 <ehddnr8563@gmail.com>
2024-01-02 17:10:01 +0900

    update: 배너 텍스트 수정

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 export default function Banner() {
   return (
     <section className='h-96 bg-yellow-900 relative'>
-      <div className='w-full h-full bg-cover bg-banner opacity-80' />
+      <div className='w-full h-full bg-cover bg-banner opacity-90' />
       <div className='absolute w-full top-10 text-center text-gray-50 drop-shadow-2xl'>
         <h2 className='text-6xl'>신발은 오늘마켓</h2>
-        <p className='text-2xl'>좋은 신발과 높은 퀄리티의 신발</p>
+        <p className='text-2xl'>
+          오늘 신을 신발은 무엇인가요? 오늘마켓에서 골라보세요!
+        </p>
       </div>
     </section>
   );
